3e6d6aa2b546eca3a5daedbc3422efa5048fea85
Saarthak <saarthakgupta08@gmail.com>
2019-09-28 17:59:13 +0530

    Project Updated

--- src/main/java/com/sg/SpringDemo/ConfigurationWithJavaAnnotationsOnly/SportsConfig.java
+++ src/main/java/com/sg/SpringDemo/ConfigurationWithJavaAnnotationsOnly/SportsConfig.java
@@ -3,19 +3,18 @@ package com.sg.SpringDemo.ConfigurationWithJavaAnnotationsOnly;
 import com.sg.SpringDemo.Coach;
 import com.sg.SpringDemo.FortuneService;
 import com.sg.SpringDemo.SadFortuneService;
-import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.ComponentScan;
-import org.springframework.context.annotation.Configuration;
-import org.springframework.context.annotation.PropertySource;
+import org.springframework.beans.factory.config.ConfigurableBeanFactory;
+import org.springframework.context.annotation.*;
 
 @Configuration
 @PropertySource("classpath:sports.properties")
-//@ComponentScan("com.sg.SpringDemo")
+//@ComponentScan("com.sg.SpringDemo") /* Component Scanning is used where whole components are scanned. */
 public class SportsConfig {
     //define a bean for our sad fortune service
 
 
     @Bean
+    @Scope(value = ConfigurableBeanFactory.SCOPE_SINGLETON)
     public FortuneService sadFortuneService(){
         return new SadFortuneService();
     }
